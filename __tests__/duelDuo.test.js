b09b9ad23d43bf6603fe657ad1c958a4002c5f21
Quade Heaps <qheapsbiz@gmail.com>
2024-03-01 12:41:43 -0700

    fixed ports

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -12,19 +12,19 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:3000")
+    await driver.get("http://localhost:8000")
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   })
 
   test("draw button displays div choices", async () => {
-    await driver.get("http://localhost:3000")
+    await driver.get("http://localhost:8000")
     await driver.findElement(By.id("draw")).click()
     await driver.wait(until.elementLocated(By.id(`choose-header`)), 3000)
     expect(await driver.findElement(By.id(`choices`)).isDisplayed()).toBe(true)
   })
   
   test("add duo button creates duo div", async () => {
-    await driver.get("http://localhost:3000")
+    await driver.get("http://localhost:8000")
     await driver.findElement(By.id("draw")).click()
     await driver.wait(until.elementLocated(By.id(`choose-header`)), 3000)
     await driver.findElement(By.className(`bot-btn`)).click()
